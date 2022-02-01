dadededa92af164c1804c4cdc49503203c3b91ac
RuFerdZ <ge.rusiru@gmail.com>
2022-02-02 01:40:09 +0530

    updated program id

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("AdGdR4tVHcxa36vEvPGh9QHsAMLAD8861puJzr6mntJj");
 
 #[program]
 pub mod solana_twitter {
